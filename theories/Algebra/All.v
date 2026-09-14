c8a28ebcecfa2e654a197dce0d6a9f35a684b63e
jwa <jwa.software@foobox.com>
2026-09-14 22:54:50 +0200

    build: scaffold the project with dune and layered theories (#1)
    * build: add dune-project with package metadata
    
    * docs: describe build commands and layer layout
    
    * build: add jwa.Core theory
    
    * build: add jwa.Tactics theory
    
    * build: add jwa.Structures theory
    
    * build: add jwa.Relations theory
    
    * build: add jwa.Data theory
    
    * build: add jwa.Logic theory
    
    * build: add jwa.Algebra theory
    
    * build: add jwa.Programming theory
    
    * build: add jwa.All umbrella theory
    
    * build: add generated opam package file

---
+++ theories/Algebra/All.v
@@ -0,0 +1,4 @@
+(* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
+
+(* Umbrella for [jwa.Algebra]: re-exports every module of the layer, so a
+   client imports the whole layer with [From jwa Require Import Algebra.All]. *)
