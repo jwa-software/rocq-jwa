76eed8fb6fede8ff2f0995723f1441660e2b0347
jwa <jwa.software@foobox.com>
2026-09-15 10:48:33 +0200

    feat: re-export Option from the Data umbrella

--- theories/Data/All.v
+++ theories/Data/All.v
@@ -2,3 +2,6 @@
 
 (* Umbrella for [jwa.Data]: re-exports every module of the layer, so a
    client imports the whole layer with [From jwa Require Import Data.All]. *)
+
+(* [Export] so a client writes [Option A], not [Option.Option A]. *)
+From jwa Require Export Data.Option.
