876a29356f0fa71c8c169fa094b9135380e61568
jwa <jwa.software@foobox.com>
2026-09-15 14:29:55 +0200

    feat: add True and False

---
+++ theories/Core/Logic.v
@@ -0,0 +1,13 @@
+(* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
+
+(* [discriminate] looks both of these up by registered name and reports
+   [not found in table: core.True.type] without them. *)
+
+Inductive True : Prop :=
+  | I : True.
+
+Inductive False : Prop := .
+
+Register True as core.True.type.
+Register False as core.False.type.
+Register I as core.True.I.
