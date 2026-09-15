36f0a0bb8d17aa70ace3a9fe1423d6e19b2088a7
jwa <jwa.software@foobox.com>
2026-09-15 15:09:31 +0200

    feat(core): add the And and Or connectives

--- theories/Core/Logic.v
+++ theories/Core/Logic.v
@@ -1,5 +1,8 @@
 (* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
 
+(* [Core.All] would be circular from inside [Core]. *)
+From jwa Require Import Core.Notations.
+
 (* [discriminate] looks both of these up by registered name and reports
    [not found in table: core.True.type] without them. *)
 
@@ -11,3 +14,20 @@ Inductive False : Prop := .
 Register True as core.True.type.
 Register False as core.False.type.
 Register I as core.True.I.
+
+Record And (A : Prop) (B : Prop) : Prop := { And_left : A ; And_right : B }.
+
+Arguments And_left  {A} {B} _.
+Arguments And_right {A} {B} _.
+
+Inductive Or (A : Prop) (B : Prop) : Prop :=
+  | Or_left  : A -> Or A B
+  | Or_right : B -> Or A B.
+
+(* The unused side is not determined by the argument. It comes from the
+   expected type, and a use that has none needs [@Or_left]. *)
+Arguments Or_left  {A} {B} a.
+Arguments Or_right {A} {B} b.
+
+Notation "A /\ B" := (And A B) : jwa_type_scope.
+Notation "A \/ B" := (Or  A B) : jwa_type_scope.
