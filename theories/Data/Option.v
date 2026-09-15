b7fa67da79b08cbd5bf1dfa32b06ba0ce001b112
jwa <jwa.software@foobox.com>
2026-09-15 11:13:43 +0200

    feat: add the Option type and Option.map (#5)
    * feat: add the Option type
    
    * feat: re-export Option from the Data umbrella
    
    * feat: add Option.map
    
    * fix: export Core.All from the Data umbrella

---
+++ theories/Data/Option.v
@@ -0,0 +1,24 @@
+(* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
+
+(* [Core.All] carries [->]: with [-noinit] a file has only what it requires. *)
+From jwa Require Import Core.All.
+
+Inductive Option (A : Type) : Type :=
+  | None : Option A
+  | Some : A -> Option A.
+
+Arguments None {A}.
+Arguments Some {A} a.
+
+(* Named after the type so operations read [Option.map]; the two coexist. *)
+Module Option.
+
+(* [forall {A : Type} {B : Type}, (A -> B) -> Option A -> Option B] *)
+Definition map := fun {A : Type} {B : Type} (f : A -> B) (o : Option A) =>
+  match o return Option B
+  with
+  | None   => None
+  | Some a => Some (f a)
+  end.
+
+End Option.
