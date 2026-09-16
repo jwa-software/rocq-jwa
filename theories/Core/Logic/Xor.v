5d530da8f184b51427a189717308bac0cc6d56a5
jwa <jwa.software@foobox.com>
2026-09-17 01:15:12 +0200

    feat: add Xor_commutativity

--- theories/Core/Logic/Xor.v
+++ theories/Core/Logic/Xor.v
@@ -1,9 +1,11 @@
 (* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
 
 (* [Core.All] would be circular from inside [Core]; [Core.Notations] reserves
-   the level that the notation below needs, [Core.Logic.Conditional] carries
-   [->], [Core.Logic.Not] carries [~]. *)
+   the level that the notation below needs, [Core.Ltac] carries the tactic
+   language, [Core.Logic.Conditional] carries [->], [Core.Logic.Not]
+   carries [~]. *)
 From jwa Require Import Core.Notations.
+From jwa Require Import Core.Ltac.
 From jwa Require Import Core.Logic.Conditional.
 From jwa Require Import Core.Logic.Not.
 
@@ -18,3 +20,24 @@ Arguments Xor_left  {A} {B} a not_b.
 Arguments Xor_right {A} {B} not_a b.
 
 Notation "A _\/_ B" := (Xor A B) : jwa_type_scope.
+
+Theorem Xor_commutativity
+  : forall (A : Prop) (B : Prop), A _\/_ B -> B _\/_ A.
+Proof.
+  (* The context gains [A] and [B]: [|- A _\/_ B -> B _\/_ A] *)
+  intros A B.
+  (* The context gains [h : A _\/_ B]: [|- B _\/_ A] *)
+  intro h.
+  (* [Xor] has two ctors with two fields each, so [h] gives two goals: one
+     with [a : A] and [not_b : ~ B], one with [not_a : ~ A] and [b : B]. *)
+  destruct h as [a not_b | not_a b].
+  - (* [Xor_right] asks for [~ B] then [A], the two fields in the other
+       order. *)
+    exact (Xor_right not_b a).
+  - (* [Xor_left] asks for [B] then [~ A]. *)
+    exact (Xor_left b not_a).
+Qed.
+
+(* Associativity is not a theorem of this module. From [~ (A _\/_ B)] and
+   [C], a proof of [A _\/_ (B _\/_ C)] has to pick a ctor, and which one is
+   right depends on whether [A] holds; no term decides that. *)
