5af02ac338ac214f5a49420fd86006e78c3a4384
jwa <jwa.software@foobox.com>
2026-09-16 09:02:08 +0200

    feat: add the Exists connective to Core (#18)
    * feat(core): reserve the existential level
    
    * feat(core): add the Exists connective
    
    * test: name the existential in the Core.Logic guard

--- test/CoreLogicAll.v
+++ test/CoreLogicAll.v
@@ -5,9 +5,10 @@
 From jwa Require Import Core.Logic.All.
 
 Definition core_logic_all_delivers
-  : forall (A : Prop) (B : Prop),
-      A /\ B -> B \/ A -> ~ A -> (A <-> B) -> False -> True
-  := fun (A : Prop) (B : Prop)
+  : forall (A : Prop) (B : Prop) (T : Type) (Q : T -> Prop),
+      A /\ B -> B \/ A -> ~ A -> (A <-> B) -> (exists t, Q t) -> False -> True
+  := fun (A : Prop) (B : Prop) (T : Type) (Q : T -> Prop)
          (_ : A /\ B) (_ : B \/ A)
          (_ : ~ A) (_ : A <-> B)
+         (_ : exists t, Q t)
          (_ : False) => I.
