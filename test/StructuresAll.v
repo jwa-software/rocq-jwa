75777b00c34afedaa67966dd3dc5671d394534a7
jwa <jwa.software@foobox.com>
2026-09-16 14:31:07 +0200

    test: guard the Structures umbrella

---
+++ test/StructuresAll.v
@@ -0,0 +1,19 @@
+(* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
+
+(* Guards [Structures.All], imported alone. It forwards [Core.All] as well as
+   the two classes, so names from both appear below. *)
+From jwa Require Import Structures.All.
+
+Definition structures_all_delivers
+  : forall (A : Type) (op : A -> A -> A) (e : A),
+      Semigroup A op -> Monoid A op e -> ~ (true = false) -> True
+  := fun (A : Type) (op : A -> A -> A) (e : A)
+         (_ : Semigroup A op) (_ : Monoid A op e) (_ : ~ (true = false)) => I.
+
+(* The projections are top-level constants, so the umbrella has to forward
+   them too. *)
+Definition structures_all_delivers_projections
+  : forall (A : Type) (op : A -> A -> A) (e : A) (m : Monoid A op e) (x : A),
+      op e x = x
+  := fun (A : Type) (op : A -> A -> A) (e : A) (m : Monoid A op e) (x : A) =>
+       Monoid_identity_left x.
