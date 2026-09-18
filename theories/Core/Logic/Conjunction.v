e3ea4b23efc876a9982d4d3fabd90740b16bdded
jwa <jwa.software@foobox.com>
2026-09-18 12:58:05 +0200

    refactor(conjunction): move the laws into Module Conjunction and state associativity as a biconditional

--- theories/Core/Logic/Conjunction.v
+++ theories/Core/Logic/Conjunction.v
@@ -1,9 +1,9 @@
 (* Copyright (c) 2026 Junzhe Wang, licensed under the MIT License. *)
 
-From jwa Require Import Core.Notations.
-From jwa Require Import Core.Ltac.
-From jwa Require Import Core.Logic.Subjunction.
 From jwa Require Import Core.Logic.Bijunction.
+From jwa Require Import Core.Logic.Subjunction.
+From jwa Require Import Core.Ltac.
+From jwa Require Import Core.Notations.
 
 Inductive Conjunction (A : Prop) (B : Prop) : Prop :=
   | Conjunction_introduction : A -> B -> Conjunction A B.
@@ -13,7 +13,12 @@ Arguments Conjunction_introduction {A} {B} a b.
 Notation "A /\ B" := (Conjunction A B)
   : jwa_type_scope.
 
-Theorem Conjunction_commutativity
+(* A module may carry the type's name; its laws read
+ * [Conjunction.commutativity].
+ *)
+Module Conjunction.
+
+Theorem commutativity
   : forall (A : Prop) (B : Prop), A /\ B -> B /\ A.
 Proof.
   intros A B.
@@ -27,115 +32,76 @@ Proof.
   - exact a.
 Qed.
 
-Lemma Conjunction_associativity_forward
-  : forall (A : Prop) (B : Prop) (C : Prop), (A /\ B) /\ C -> A /\ (B /\ C).
+Theorem associativity
+  : forall (A : Prop) (B : Prop) (C : Prop), (A /\ B) /\ C <-> A /\ (B /\ C).
 Proof.
   intros A B C.
-  intro h.
-  destruct h  as [ab c].
-  destruct ab as [a b].
   split.
-  - exact a.
-  - split.
-    + exact b.
+  - intro h.
+    destruct h  as [ab c].
+    destruct ab as [a b].
+    split.
+    + exact a.
+    + split.
+      * exact b.
+      * exact c.
+  - intro h.
+    destruct h  as [a bc].
+    destruct bc as [b c].
+    split.
+    + split.
+      * exact a.
+      * exact b.
     + exact c.
 Qed.
 
-Lemma Conjunction_associativity_backward
-  : forall (A : Prop) (B : Prop) (C : Prop), A /\ (B /\ C) -> (A /\ B) /\ C.
-Proof.
-  intros A B C.
-  intro h.
-  destruct h  as [a bc].
-  destruct bc as [b c].
-  split.
-  - split.
-    + exact a.
-    + exact b.
-  - exact c.
-Qed.
-
-Theorem Conjunction_associativity
-  : forall (A : Prop) (B : Prop) (C : Prop),
-      ((A /\ B) /\ C -> A /\ (B /\ C)) /\ (A /\ (B /\ C) -> (A /\ B) /\ C).
-Proof.
-  intros A B C.
-  split.
-  - exact (Conjunction_associativity_forward  A B C).
-  - exact (Conjunction_associativity_backward A B C).
-Qed.
-
-Lemma Subjunction_currying_forward
-  : forall (A : Prop) (B : Prop) (C : Prop), (A /\ B -> C) -> A -> B -> C.
-Proof.
-  intros A B C.
-  intro f.
-  intro a.
-  intro b.
-  apply f.
-  split.
-  - exact a.
-  - exact b.
-Qed.
-
-Lemma Subjunction_currying_backward
-  : forall (A : Prop) (B : Prop) (C : Prop), (A -> B -> C) -> A /\ B -> C.
-Proof.
-  intros A B C.
-  intro f.
-  intro h.
-  destruct h as [a b].
-  apply f.
-  - exact a.
-  - exact b.
-Qed.
-
-Theorem Subjunction_currying
+(* Currying: a proof from a pair is a proof from the first that returns a
+ * proof from the second.
+ *)
+Theorem currying
   : forall (A : Prop) (B : Prop) (C : Prop), (A /\ B -> C) <-> (A -> B -> C).
 Proof.
   intros A B C.
   split.
-  - exact (Subjunction_currying_forward  A B C).
-  - exact (Subjunction_currying_backward A B C).
+  - intro f.
+    intro a.
+    intro b.
+    apply f.
+    split.
+    + exact a.
+    + exact b.
+  - intro f.
+    intro h.
+    destruct h as [a b].
+    apply f.
+    + exact a.
+    + exact b.
 Qed.
 
-Lemma Subjunction_distributivity_over_Conjunction_forward
-  : forall (A : Prop) (B : Prop) (C : Prop),
-      (A -> B /\ C) -> (A -> B) /\ (A -> C).
-Proof.
-  intros A B C.
-  intro f.
-  split; intro a; destruct (f a) as [b c].
-  - exact b.
-  - exact c.
-Qed.
-
-Lemma Subjunction_distributivity_over_Conjunction_backward
-  : forall (A : Prop) (B : Prop) (C : Prop),
-      (A -> B) /\ (A -> C) -> A -> B /\ C.
-Proof.
-  intros A B C.
-  intro h.
-  destruct h as [ab ac].
-  intro a.
-  split.
-  - apply ab.
-    exact a.
-  - apply ac.
-    exact a.
-Qed.
-
-Theorem Subjunction_distributivity_over_Conjunction
+(* The universal property of [/\] as a product: a proof of [B /\ C] from [A]
+ * is a pair of proofs from [A], one of [B] and one of [C].
+ *)
+Theorem universality
   : forall (A : Prop) (B : Prop) (C : Prop),
       (A -> B /\ C) <-> (A -> B) /\ (A -> C).
 Proof.
   intros A B C.
   split.
-  - exact (Subjunction_distributivity_over_Conjunction_forward  A B C).
-  - exact (Subjunction_distributivity_over_Conjunction_backward A B C).
+  - intro f.
+    split; intro a; destruct (f a) as [b c].
+    + exact b.
+    + exact c.
+  - intro h.
+    destruct h as [ab ac].
+    intro a.
+    split.
+    + apply ab.
+      exact a.
+    + apply ac.
+      exact a.
 Qed.
 
-Theorem Conjunction_congruence
+Theorem congruence
   : forall (A1 : Prop) (A2 : Prop) (B1 : Prop) (B2 : Prop),
       (A1 <-> A2) -> (B1 <-> B2) -> (A1 /\ B1 <-> A2 /\ B2).
 Proof.
@@ -144,7 +110,8 @@ Proof.
   destruct a as [a12 a21].
   destruct b as [b12 b21].
   (* [Bijunction] has one ctor with two fields, so the goal splits into two
-     goals: [|- A1 /\ B1 -> A2 /\ B2] and [|- A2 /\ B2 -> A1 /\ B1]. *)
+   * goals: [|- A1 /\ B1 -> A2 /\ B2] and [|- A2 /\ B2 -> A1 /\ B1].
+   *)
   split.
   - (* [h : A1 /\ B1]: [|- A2 /\ B2] *)
     intro h.
@@ -165,3 +132,5 @@ Proof.
     + apply b21.
       exact b2.
 Qed.
+
+End Conjunction.
