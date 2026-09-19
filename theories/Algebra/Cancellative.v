583db67184da23509eefe48dfe009a384c299305
jwa <jwa.software@foobox.com>
2026-09-19 11:52:35 +0200

    refactor(algebra): reorder the variables of the right cancellation law

--- theories/Algebra/Cancellative.v
+++ theories/Algebra/Cancellative.v
@@ -6,4 +6,4 @@ From jwa Require Import Core.Class.
 Class Cancellative {A : Type} (op : A -> A -> A) : Prop :=
   { cancellation
     : forall (x : A) (y : A) (z : A),
-      (op x y = op x z -> y = z) /\ (op x z = op y z -> x = y) }.
+      (op x y = op x z -> y = z) /\ (op x y = op z y -> x = z) }.
