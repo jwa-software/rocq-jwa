336d09f02b1ae2ea31a39966d5a8fd2d6be7f04c
jwa <jwa.software@foobox.com>
2026-09-20 22:31:03 +0200

    feat(tactics): comma-separated premises, in either order

--- test/TacticsAll.v
+++ test/TacticsAll.v
@@ -7,14 +7,21 @@ Theorem tactics_all_delivers_modus_ponens
   : forall (A : Prop) (B : Prop) . (A -> B) -> A -> B.
 Proof.
   intros A B hab ha.
-  modus ponens hab ha.
+  modus ponens hab, ha.
+Qed.
+
+Theorem tactics_all_delivers_modus_ponens_reversed
+  : forall (A : Prop) (B : Prop) . (A -> B) -> A -> B.
+Proof.
+  intros A B hab ha.
+  modus ponens ha, hab.
 Qed.
 
 Theorem tactics_all_delivers_modus_ponens_as
   : forall (A : Prop) (B : Prop) (C : Prop) . (A -> B /\ C) -> A -> B.
 Proof.
   intros A B C hab ha.
-  modus ponens hab ha as [b c].
+  modus ponens hab, ha as [b c].
   exact b.
 Qed.
 
@@ -22,14 +29,14 @@ Theorem tactics_all_delivers_modus_ponendo_ponens
   : forall (A : Prop) (B : Prop) . (A -> B) -> A -> B.
 Proof.
   intros A B hab ha.
-  modus ponendo ponens hab ha.
+  modus ponendo ponens hab, ha.
 Qed.
 
 Theorem tactics_all_delivers_modus_ponendo_ponens_as
   : forall (A : Prop) (B : Prop) . (A -> B) -> A -> B.
 Proof.
   intros A B hab ha.
-  modus ponendo ponens hab ha as h.
+  modus ponendo ponens hab, ha as h.
   exact h.
 Qed.
 
@@ -37,21 +44,21 @@ Theorem tactics_all_delivers_modus_ponens_implicit
   : forall (m : Nat) (n : Nat) . Nat.compare m n = Lt -> Nat.LessThan m n.
 Proof.
   intros m n c.
-  modus ponens Nat.comparison.strict.forward.specification c.
+  modus ponens Nat.comparison.strict.forward.specification, c.
 Qed.
 
 Theorem tactics_all_delivers_modus_tollens
   : forall (A : Prop) (B : Prop) . (A -> B) -> ~ B -> ~ A.
 Proof.
   intros A B hab hnb.
-  modus tollens hab hnb.
+  modus tollens hab, hnb.
 Qed.
 
 Theorem tactics_all_delivers_modus_tollendo_tollens
   : forall (A : Prop) (B : Prop) . (A -> B) -> ~ B -> ~ A.
 Proof.
   intros A B hab hnb.
-  modus tollendo tollens hab hnb as na.
+  modus tollendo tollens hab, hnb as na.
   exact na.
 Qed.
 
@@ -59,14 +66,14 @@ Theorem tactics_all_delivers_modus_tollendo_ponens
   : forall (A : Prop) (B : Prop) . A \/ B -> ~ A -> B.
 Proof.
   intros A B hor hna.
-  modus tollendo ponens hor hna.
+  modus tollendo ponens hor, hna.
 Qed.
 
 Theorem tactics_all_delivers_modus_tollendo_ponens_as
   : forall (A : Prop) (B : Prop) . A \/ B -> ~ A -> B.
 Proof.
   intros A B hor hna.
-  modus tollendo ponens hor hna as b.
+  modus tollendo ponens hor, hna as b.
   exact b.
 Qed.
 
@@ -74,13 +81,13 @@ Theorem tactics_all_delivers_modus_ponendo_tollens
   : forall (A : Prop) (B : Prop) . ~ (A /\ B) -> A -> ~ B.
 Proof.
   intros A B hn ha.
-  modus ponendo tollens hn ha.
+  modus ponendo tollens hn, ha.
 Qed.
 
 Theorem tactics_all_delivers_modus_ponendo_tollens_sejunction
   : forall (A : Prop) (B : Prop) . A _\/_ B -> A -> ~ B.
 Proof.
   intros A B hs ha.
-  modus ponendo tollens hs ha as nb.
+  modus ponendo tollens hs, ha as nb.
   exact nb.
 Qed.
--- theories/Tactics/Modus.v
+++ theories/Tactics/Modus.v
@@ -8,13 +8,18 @@ From jwa Require Import Core.Ltac.
  * form also comes with [as <p>], which puts the conclusion into the context
  * under the intro pattern <p> instead of closing the goal.
  *
- *   modus ponens          <Hab> <Ha>     A -> B, A |- B
+ *   modus ponens          <H1>, <H2>    A -> B, A |- B
  *
- *   modus tollens         <Hab> <Hnb>    A -> B, ~ B |- ~ A
+ *   modus tollens         <H1>, <H2>    A -> B, ~ B |- ~ A
  *
- *   modus tollendo ponens <Hor> <Hna>    A \/ B, ~ A |- B
+ *   modus tollendo ponens <H1>, <H2>    A \/ B, ~ A |- B
  *
- *   modus ponendo tollens <Hn>  <Ha>     ~ (A /\ B), A |- ~ B
+ *   modus ponendo tollens <H1>, <H2>    ~ (A /\ B), A |- ~ B
+ *
+ * The two premises are separated by a comma and may be given in either order:
+ * each body tries the other way round when the first does not apply. In the
+ * bare form the goal decides; under [as] there is no goal to decide, so if
+ * both orders happen to type check the first one stands.
  *
  * The first two answer to their full names as well, [modus ponendo ponens]
  * and [modus tollendo tollens].
@@ -28,48 +33,60 @@ From jwa Require Import Core.Ltac.
  * implicit binders have nothing yet to fix them.
  *)
 
-Tactic Notation "modus" "ponens" uconstr(Hab) uconstr(Ha) :=
-  exact (Hab Ha).
+Tactic Notation "modus" "ponens" uconstr(H1) "," uconstr(H2) :=
+  first [ exact (H1 H2) | exact (H2 H1) ].
 
-Tactic Notation "modus" "ponens" uconstr(Hab) uconstr(Ha) "as" simple_intropattern(p) :=
-  pose proof (Hab Ha) as p.
-
-Tactic Notation "modus" "ponendo" "ponens" uconstr(Hab) uconstr(Ha) :=
-  exact (Hab Ha).
-
-Tactic Notation "modus" "ponendo" "ponens" uconstr(Hab) uconstr(Ha)
+Tactic Notation "modus" "ponens" uconstr(H1) "," uconstr(H2)
     "as" simple_intropattern(p) :=
-  pose proof (Hab Ha) as p.
+  first [ pose proof (H1 H2) as p | pose proof (H2 H1) as p ].
 
-Tactic Notation "modus" "tollens" uconstr(Hab) uconstr(Hnb) :=
-  exact (Negation.contraposition Hab Hnb).
+Tactic Notation "modus" "ponendo" "ponens" uconstr(H1) "," uconstr(H2) :=
+  first [ exact (H1 H2) | exact (H2 H1) ].
 
-Tactic Notation "modus" "tollens" uconstr(Hab) uconstr(Hnb) "as" simple_intropattern(p) :=
-  pose proof (Negation.contraposition Hab Hnb) as p.
-
-Tactic Notation "modus" "tollendo" "tollens" uconstr(Hab) uconstr(Hnb) :=
-  exact (Negation.contraposition Hab Hnb).
-
-Tactic Notation "modus" "tollendo" "tollens" uconstr(Hab) uconstr(Hnb)
+Tactic Notation "modus" "ponendo" "ponens" uconstr(H1) "," uconstr(H2)
     "as" simple_intropattern(p) :=
-  pose proof (Negation.contraposition Hab Hnb) as p.
+  first [ pose proof (H1 H2) as p | pose proof (H2 H1) as p ].
 
-Tactic Notation "modus" "tollendo" "ponens" uconstr(Hor) uconstr(Hna) :=
-  exact (Negation.elimination.of.disjunction Hor Hna).
+Tactic Notation "modus" "tollens" uconstr(H1) "," uconstr(H2) :=
+  first [ exact (Negation.contraposition H1 H2)
+        | exact (Negation.contraposition H2 H1) ].
 
-Tactic Notation "modus" "tollendo" "ponens" uconstr(Hor) uconstr(Hna)
+Tactic Notation "modus" "tollens" uconstr(H1) "," uconstr(H2)
     "as" simple_intropattern(p) :=
-  pose proof (Negation.elimination.of.disjunction Hor Hna) as p.
+  first [ pose proof (Negation.contraposition H1 H2) as p
+        | pose proof (Negation.contraposition H2 H1) as p ].
 
-Tactic Notation "modus" "ponendo" "tollens" uconstr(Hn) uconstr(Ha) :=
+Tactic Notation "modus" "tollendo" "tollens" uconstr(H1) "," uconstr(H2) :=
+  first [ exact (Negation.contraposition H1 H2)
+        | exact (Negation.contraposition H2 H1) ].
+
+Tactic Notation "modus" "tollendo" "tollens" uconstr(H1) "," uconstr(H2)
+    "as" simple_intropattern(p) :=
+  first [ pose proof (Negation.contraposition H1 H2) as p
+        | pose proof (Negation.contraposition H2 H1) as p ].
+
+Tactic Notation "modus" "tollendo" "ponens" uconstr(H1) "," uconstr(H2) :=
+  first [ exact (Negation.elimination.of.disjunction H1 H2)
+        | exact (Negation.elimination.of.disjunction H2 H1) ].
+
+Tactic Notation "modus" "tollendo" "ponens" uconstr(H1) "," uconstr(H2)
+    "as" simple_intropattern(p) :=
+  first [ pose proof (Negation.elimination.of.disjunction H1 H2) as p
+        | pose proof (Negation.elimination.of.disjunction H2 H1) as p ].
+
+Tactic Notation "modus" "ponendo" "tollens" uconstr(H1) "," uconstr(H2) :=
   first
-    [ exact (Negation.exclusion.of.conjunction Hn Ha)
-    | exact (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction Hn) Ha) ].
+    [ exact (Negation.exclusion.of.conjunction H1 H2)
+    | exact (Negation.exclusion.of.conjunction H2 H1)
+    | exact (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction H1) H2)
+    | exact (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction H2) H1) ].
 
-Tactic Notation "modus" "ponendo" "tollens" uconstr(Hn) uconstr(Ha)
+Tactic Notation "modus" "ponendo" "tollens" uconstr(H1) "," uconstr(H2)
     "as" simple_intropattern(p) :=
   first
-    [ pose proof (Negation.exclusion.of.conjunction Hn Ha) as p
+    [ pose proof (Negation.exclusion.of.conjunction H1 H2) as p
+    | pose proof (Negation.exclusion.of.conjunction H2 H1) as p
     | pose proof
-        (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction Hn) Ha)
-        as p ].
+        (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction H1) H2) as p
+    | pose proof
+        (Negation.exclusion.of.conjunction (Sejunction.exclusion.of.conjunction H2) H1) as p ].
