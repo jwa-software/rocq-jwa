1c3baa3960092039f2fcc65f6e9e96064ee8b5c2
jwa <jwa.software@foobox.com>
2026-09-21 18:17:25 +0200

    feat(number): the gcd divides both and is the greatest

--- test/DataNumberAll.v
+++ test/DataNumberAll.v
@@ -31,6 +31,25 @@ Definition data_number_all_delivers_gcd_recurrence
       = NatWithZero.gcd (NatWithZero.Positive q) (NatWithZero.modulo a q)
   := NatWithZero.gcd.recurrence.
 
+Definition data_number_all_delivers_gcd_common
+  : forall (b : NatWithZero) (a : NatWithZero) .
+      NatWithZero.Divides (NatWithZero.gcd a b) a
+      /\ NatWithZero.Divides (NatWithZero.gcd a b) b
+  := NatWithZero.gcd.common.
+
+Definition data_number_all_delivers_gcd_projections
+  : forall (a : NatWithZero) (b : NatWithZero) .
+      NatWithZero.Divides (NatWithZero.gcd a b) a
+      /\ NatWithZero.Divides (NatWithZero.gcd a b) b
+  := fun (a : NatWithZero) (b : NatWithZero) .
+       Conjunction_introduction (NatWithZero.gcd.left a b) (NatWithZero.gcd.right a b).
+
+Definition data_number_all_delivers_gcd_greatest
+  : forall (b : NatWithZero) (a : NatWithZero) (d : NatWithZero) .
+      NatWithZero.Divides d a ->
+      NatWithZero.Divides d b -> NatWithZero.Divides d (NatWithZero.gcd a b)
+  := NatWithZero.gcd.greatest.
+
 Definition data_number_all_delivers_well_founded_magnitude
   : forall (x : Integer) .
       Accessible (Preimage Integer.abs NatWithZero.LessThan) x
