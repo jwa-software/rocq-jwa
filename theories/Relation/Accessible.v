6c9ebaea0d2397b295e323aaaa7e830ebd2c1abe
jwa <jwa.software@foobox.com>
2026-09-22 11:41:47 +0200

    refactor(relation): recursion.independence step by step

--- theories/Relation/Accessible.v
+++ theories/Relation/Accessible.v
@@ -78,24 +78,138 @@ Qed.
 
 (* recursion.independence *)
 Theorem independence
-  : forall {A : Type} {P : A -> Type} {R : A -> A -> Prop}
-      {step : forall (x : A) . (forall (y : A) . R y x -> P y) -> P x} .
-      Extensional step ->
+  : forall {A : Type} {R : A -> A -> Prop} {P : A -> Type} {step : Descent.Step R P} .
+      Descent.Extensional step ->
       forall (x : A) (a : Accessible R x) (b : Accessible R x) .
       recursion step x a = recursion step x b.
 Proof.
-  intros A P R step extensional x a b.
-  apply (recursion
-           (R := R)
-           (P := fun (c : A) .
-                 forall (u : Accessible R c) (v : Accessible R c) .
-                   recursion step c u = recursion step c v)).
-  - intros c recurse u v.
-    rewrite (unfolding step c u) in |- *.
-    rewrite (unfolding step c v) in |- *.
-    apply extensional.
-    intros y r.
-    exact (recurse y r (Accessible.descend u r) (Accessible.descend v r)).
+  (* [A : Type]
+   * [R : A -> A -> Prop]
+   * [_P : A -> Type]
+   * [step : Descent.Step R _P]
+   * [extensional : Descent.Extensional step]
+   * [x : A]
+   * [a : Accessible R x]
+   * [b : Accessible R x]
+   * :
+   * [|- recursion step x a = recursion step x b]
+   *)
+  intros A R _P step extensional x a b.
+
+  (* [P := fun (x : A) .
+   *       forall (a : Accessible R x) (b : Accessible R x) .
+   *         recursion step x a = recursion step x b]
+   *)
+  set (P := fun (x : A) .
+            forall (a : Accessible R x) (b : Accessible R x) .
+              recursion step x a = recursion step x b).
+
+  (* The context gains
+   * [recursor : Descent.Step R P -> forall (x : A) . Accessible R x -> P x]
+   *)
+  pose proof (recursion (R := R) (P := P)) as recursor.
+
+  (* [at 2] is the occurrence of [P] in the conclusion; the one in the
+   * premise stays folded.
+   * [recursor : Descent.Step R P ->
+   *             forall (x : A) . Accessible R x ->
+   *             forall (a : Accessible R x) (b : Accessible R x) .
+   *               recursion step x a = recursion step x b]
+   *)
+  unfold P at 2 in recursor.
+
+  (* [|- Descent.Step R P]
+   * [|- Accessible R x]
+   *)
+  apply recursor.
+
+  - clear a b x.
+
+    (* [|- forall (x : A) . (forall (y : A) . R y x -> P y) -> P x] *)
+    simplify Descent.Step in |- *.
+
+    (* [x : A]
+     * [recurse : forall (y : A) . R y x -> P y]
+     * :
+     * [|- P x]
+     *)
+    intros x recurse.
+
+    (* [|- forall (a : Accessible R x) (b : Accessible R x) .
+     *      recursion step x a = recursion step x b]
+     *)
+    simplify P in |- *.
+
+    (* [a : Accessible R x]
+     * [b : Accessible R x]
+     * :
+     * [|- recursion step x a = recursion step x b]
+     *)
+    intros a b.
+
+    (* [|- step x (fun (y : A) (r : R y x) . recursion step y (descend a r))
+     *  = recursion step x b]
+     *)
+    rewrite (unfolding step x a) in |- *.
+
+    (* [|- step x (fun (y : A) (r : R y x) . recursion step y (descend a r))
+     *  = step x (fun (y : A) (r : R y x) . recursion step y (descend b r))]
+     *)
+    rewrite (unfolding step x b) in |- *.
+
+    (* [f := fun (y : A) (r : R y x) . recursion step y (descend a r)]
+     * :
+     * [|- step x f
+     *  = step x (fun (y : A) (r : R y x) . recursion step y (descend b r))]
+     *)
+    set (f := fun (y : A) (r : R y x) . recursion step y (descend a r)).
+
+    (* [g := fun (y : A) (r : R y x) . recursion step y (descend b r)]
+     * :
+     * [|- step x f = step x g]
+     *)
+    set (g := fun (y : A) (r : R y x) . recursion step y (descend b r)).
+
+    (* The context gains
+     * [H : forall (f : forall (y : A) . R y x -> _P y)
+     *             (g : forall (y : A) . R y x -> _P y) .
+     *        (forall (y : A) (r : R y x) . f y r = g y r) -> step x f = step x g]
+     *)
+    pose proof (extensional x) as H.
+
+    (* The context gains
+     * [H' : (forall (y : A) (r : R y x) . f y r = g y r) -> step x f = step x g]
+     *)
+    pose proof (H f g) as H'.
+
+    assert (pointwise : forall (y : A) (r : R y x) . f y r = g y r).
+    {
+      intros y r.
+
+      (* [|- recursion step y (descend a r)
+       *  = recursion step y (descend b r) ]
+       *)
+      simplify f, g in |- *.
+
+      (* [|- recursion step y a'
+       *  = recursion step y b' ]
+       *)
+      set (a' := descend a r).
+      set (b' := descend b r).
+
+      (* The context gains [h : P y] *)
+      pose proof (recurse y r) as h.
+
+      (* [h : forall (a : Accessible R y) (b : Accessible R y) .
+       *      recursion step y a = recursion step y b]
+       *)
+      simplify P in h.
+
+      exact (h a' b').
+    }
+
+    modus ponens H', pointwise.
+
   - exact a.
 Qed.
 
