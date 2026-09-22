e56506a45b61c242dbcd1d4aa44e298d46b2bfba
jwa <jwa.software@foobox.com>
2026-09-22 19:46:02 +0200

    feat(number): Data.Number.All exports Data.Option

--- theories/Data/Number/All.v
+++ theories/Data/Number/All.v
@@ -4,12 +4,10 @@
  * client imports them with [From jwa Require Import Data.Number.All].
  *)
 
-(* The modules below only [Import] [Core.All], so the open scope reaches a
- * client of this umbrella only from here.
- *)
 From jwa Require Export Core.All.
 
 From jwa Require Export Data.Number.Integer.
 From jwa Require Export Data.Number.Nat.
 From jwa Require Export Data.Number.NatWithZero.
 From jwa Require Export Data.Number.Rational.
+From jwa Require Export Data.Option.
